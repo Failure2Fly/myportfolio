4cb30a525139764760534e14ff10f1233ede869b
Devin Blankenship <failure2fly@gmail.com>
2017-04-29 21:53:51 -0400

    materialize setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Navbar from './Navbar'
+import Skills from './Skills'
+import Work from './Work'
+import About from './About'
+import Contact from './Contact'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <Navbar />
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+    constructor(props) {
+        super(props)
+
+
+    }
+
+
+  render() {
+    return <div className="navbar-fixed">
+    <nav>
+    <div className="nav-wrapper">
+      <a className="brand-logo">DEVIN BLANKENSHIP</a>
+      <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
+      <ul className="right hide-on-med-and-down">
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#work">Work</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <ul className="side-nav" id="mobile-demo">
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#work">Work</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </div>
+  </nav>
+  </div>
+    
+  }
+}
+
+//  $( document ).ready(function(){
+//      $(".button-collapse").sideNav()
+//  })
+
+
+export default Navbar;
