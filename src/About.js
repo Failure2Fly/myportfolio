6addabe7f802a628e2da54f8d56c2dd5618c6a14
Devin Blankenship <failure2fly@gmail.com>
2017-05-02 02:26:18 -0400

    changes to mobile

--- src/About.js
+++ src/About.js
@@ -17,8 +17,8 @@ class About extends Component {
     <div className="row">       
             <div className="col s12 m6">
                 <h2 className="about-me">
-                  <span>I am a Front-End Engineer living in Indianapolis, Indiana. I love myself a good puzzle and the "Eureka!" moment that follows when I solve it. I believe you should always be learning and finding better solutions to those problems. I enjoy being as creative as I can while keeping aligned with a simple product that will be user friendly for all.</span><br/><br/>
-                  <span>I love The Simpsons, playing board games and challenging myself to the occasional escape room once in a while.</span>
+                  <span className="about-me-text">I am a Front-End Engineer living in Indianapolis, Indiana. I love myself a good puzzle and the "Eureka!" moment that follows when I solve it. I believe you should always be learning and finding better solutions to those problems. I enjoy being as creative as I can while keeping aligned with a simple product that will be user friendly for all.</span><br/><br/>
+                  <span className="about-me-text">I love The Simpsons, playing board games and challenging myself to the occasional escape room once in a while.</span>
                 </h2>
             </div>
         </div>
--- src/Contact.js
+++ src/Contact.js
@@ -5,40 +5,65 @@ class Contact extends Component {
     return <div className="contact-background">
     <a name="contact"></a>
       <div className="row">
-            <div className="col s6 m4">         
+            <div className="col s12 m4 contact-info">
+                <h1 className="contact-title">Contact Me</h1>
+                <ul className="collapsible" data-collapsible="accordion">
+                  <li>
+                    <div className="collapsible-header"><i className="material-icons">stay_primary_portrait</i>Phone</div>
+                    <div className="collapsible-body"><span>(260)-348-6951</span></div>
+                  </li>
+                  <li>
+                    <div className="collapsible-header"><i className="material-icons">description</i>Resume</div>
+                    <div className="collapsible-body"><span></span></div>
+                  </li>
+                  <li>
+                    <div className="collapsible-header"><i className="material-icons">mail</i>Email</div>
+                    <div className="collapsible-body"><span>frontdevdevin@gmail.com</span></div>
+                  </li>
+                </ul>
             </div>
             <form action="https://formspree.io/frontdevdevin@gmail.com"
-      method="POST" className="col s12 m8">
+              method="POST" className="col s12 m8">
               <div className="row contact-form">
-
-                <div className="input-field col s6 m6 form-input">
-                  <i className="material-icons prefix">account_circle</i>
-                  <input id="icon_prefix" type="text" className="validate" name="Name" placeholder="Name"/>
-                  <label for="icon_prefix"></label>
+              
+                <div className="input-field col s6 m6">
+                  <div className="form-input">
+                    <i className="material-icons prefix">account_circle</i>
+                    <input id="icon_prefix" type="text" className="validate" name="Name" placeholder="Name"/>
+                    <label for="icon_prefix"></label>
+                  </div>
                 </div>
                 <div className="input-field col s6 m6">
-                  <i className="material-icons prefix">phone</i>
-                  <input id="icon_telephone" type="tel" className="validate" name="Phone_number" placeholder="Telephone" />
-                  <label for="icon_telephone"></label>
+                  <div className="form-input">
+                    <i className="material-icons prefix">phone</i>
+                    <input id="icon_telephone" type="tel" className="validate" name="Phone_number" placeholder="Telephone" />
+                    <label for="icon_telephone"></label>
+                  </div>
                 </div>
-
+     
                 <div className="row">
                   <div className="input-field col s12">
-                    <i className="material-icons prefix">mode_edit</i>
-                    <textarea id="icon_prefix2" className="materialize-textarea" name="Message" placeholder="Message"></textarea>
-                    <label for="icon_prefix2"></label>
+                    <div className="form-input">
+                      <i className="material-icons prefix">mode_edit</i>
+                      <textarea id="icon_prefix2" className="materialize-textarea" name="Message" placeholder="Message"></textarea>
+                      <label for="icon_prefix2"></label>
+                    </div>
                   </div>
                 </div>
                 
                 <div className="input-field col s12 m6">
-                  <i className="material-icons prefix">account_circle</i>
-                  <input id="icon_prefix" type="text" className="validate" name="_Replyto" placeholder="Email"/>
-                  <label for="icon_prefix"></label>
+                  <div className="form-input">
+                    <i className="material-icons prefix">account_circle</i>
+                    <input id="icon_prefix" type="text" className="validate" name="_Replyto" placeholder="Email"/>
+                    <label for="icon_prefix"></label>
+                  </div>
                 </div>
                 <div className="input-field col s12 m6 form-submit-button">
-                  <button className="btn waves-effect waves-light" type="submit" name="action" value="send">Submit
-                    <i className="material-icons right">send</i>
-                  </button>
+                  <div className="form-input">
+                    <button className="btn waves-effect waves-light" type="submit" name="action" value="send">Submit
+                      <i className="material-icons right">send</i>
+                    </button>
+                  </div>
                 </div>
 
               </div>
